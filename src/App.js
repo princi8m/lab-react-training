05fd6fdb7527a755fde1361d3c9a28460235b985
MP <princi8@gmail.comm>
2021-02-23 00:04:16 +0100

    v1 with problems

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IdCard from './Components/IdCard';
+import Greetings from './Components/Greetings';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,40 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <h2>IdCard</h2>
+
+<p>
+      <div class="box1">
+      <IdCard
+       lastName='Doe'
+       firstName='John'
+       gender='male'
+       height={178}
+       birth={new Date("1992-07-14")}
+       picture="https://randomuser.me/api/portraits/men/44.jpg"
+     />
+     </div>
+
+     <div class="box1">
+      <IdCard
+      lastName='Delores '
+      firstName='Obrien'
+      gender='female'
+      height={172}
+      birth={new Date("1988-05-11")}
+      picture="https://randomuser.me/api/portraits/women/44.jpg"
+    />
+     </div>
+   
+
+     <h2>Greetings</h2>
+        <Greetings lang="en">Simon</Greetings>
+        <Greetings lang="it">Michele</Greetings>
+
+        </p>
+
     </div>
   );
 }
